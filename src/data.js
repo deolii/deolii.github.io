213608b77325cf840b47f2f2c20a2e612016eca3
Adeola <adeolasarah001@gmail.com>
2023-01-14 00:32:11 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+    {
+      title: "Portfolio",
+      subtitle: "Figma",
+      description:
+        "A group portfolio website in different screen sizes",
+      image: "./a.PNG",
+      link: "https://www.figma.com/file/Bw4VKd8dk44vzotJeXnIXm/Portfolio-Project?node-id=20%3A163&t=4zAAyetHf0MhNshD-0",
+    },
+    
+    {
+      title: "Personal Website",
+      subtitle: "Figma, Google Workspace Tools",
+      description:
+        "Home Page for a personal website.",
+      image: "./c.PNG",
+      link: "https://www.figma.com/file/H7wxSlDV1QCv128cFv0s3n/My-Portfolio-Website?node-id=65%3A46&t=NHtWXOxanBORVzhf-0",
+    },
+    {
+      title: "Corva",
+      subtitle: "Figma, Miro, Google Workspace Tools",
+      description:
+        "A web-based application for getting access to varieties of insurance packages.",
+      image: "./b.PNG",
+      link: "https://www.figma.com/file/7ON3K6RWquz5Lls2b61NTx/Corva-(Hi-Fi)?node-id=1075%3A2595&t=ID4Fr5BYA4eHyVhp-0",
+    },
+    {
+      title: "Shuttlers Mobile Screens",
+      subtitle: "Figma",
+      description:
+        "User interface screen of Shuttlers mobile application",
+      image: "./e.PNG",
+      link: "https://www.figma.com/file/hNBGppHy81qWwr8bxDG6jR/Shuttlers-Re-design?node-id=43%3A37&t=z53ZCloERyChwQrJ-0",
+    },
+  ];
+
+  export const skills = [
+   "Figma", "Prototyping", "Human Centered Design", "Miro", "Notion", "Adobe XD", "Google Workspace Tools", "Adobe Photoshop"
+    
+  ] ;
+
+  export const testimonials = [
+    {
+      quote: "Working with Adeola was like a breath of fresh air! Having someone visually articulate product requirements and user stories into mockups and prototypes was refreshing. It also helps that she is reliable, accountable, and enthusiastic about learning and researching ways to improve user experiences.",
+      name: "Adaobi Nwonye",
+      company:
+        "Africa Resource Company (ARC)",
+      image: "./Adaobi.jpg",
+    },
+    
+  ];
